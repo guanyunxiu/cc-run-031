3931cbfbbd5816c406e8e8c011c7ab1ee3292861
guanyunxiu <374427398@qq.com>
2026-09-20 09:48:27 +0800

    chore: snapshot code031/B

---
+++ test/index.ts
@@ -0,0 +1,14 @@
+// 测试入口：导入所有用例并执行
+import './basic.test';
+import './join.test';
+import './aggregate.test';
+import './subquery.test';
+import './null.test';
+import './types.test';
+import './features.test';
+import './idb.test';
+import { runAll } from './harness';
+
+export async function run(): Promise<number> {
+  return runAll();
+}
